4f6cfb9f2d35234a77e680c9e0c58d3a4b242451
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-09-05 21:03:41 +0300

    feat: add inventory model

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models import Q
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from ..catalog.models import Product
+
+
+
+class Inventory(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(quantity__gte=0),
+                name="inventory_quantity_gte_0"
+            )
+        ]
--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "apps.catalog",
+    "apps.inventory",
+    "apps.orders",
+    "apps.payments",
 ]
 
 MIDDLEWARE = [
