e7af5af565abfa98e115dbc64d1d353a155996aa
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-09-05 20:21:14 +0300

    feat: add Product model

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models import Q
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+from decimal import Decimal
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2, 
+        validators=[MinValueValidator(Decimal("0.01"))],
+
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(price__gte=Decimal("0.01")),
+                name="product_price_gte_001"
+            )
+        ]
