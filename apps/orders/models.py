05ac978f4eb550ae6f59ef6c20d6a962b9101d1d
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-09-07 23:00:32 +0300

    feat: add cart and cart item models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.db.models import Q
+from django.conf import settings
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey("catalog.Product", on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        constraints = [
+        models.CheckConstraint(
+            condition=Q(quantity__gte=1),
+            name="cart_item_quantity_gte_1"
+        ),
+        models.UniqueConstraint(
+            fields=["product", "cart"],
+            name="unique_cart_product"
+        )
+    ]
