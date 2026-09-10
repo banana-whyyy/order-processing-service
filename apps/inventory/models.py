5a4a48b58035bea4bdc47c69553d6d9abcc77d3e
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-09-10 21:44:08 +0300

    fix: import product in inventory model

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -2,12 +2,9 @@ from django.db import models
 from django.db.models import Q
 from django.core.validators import MinValueValidator
 
-from ..catalog.models import Product
-
-
 
 class Inventory(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    product = models.OneToOneField("catalog.Product", on_delete=models.CASCADE)
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
 
     class Meta:
