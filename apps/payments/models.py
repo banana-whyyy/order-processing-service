317ff7043fc7074829c16a85ea83b1ea761a64d1
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-09-10 21:54:34 +0300

    feat: add payment models

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "pending", "Pending"
+        PAID = "paid", "Paid"
+        FAILED = "failed", "Failed"
+        CANCELLED = "cancelled", "Cancelled"
+        EXPIRED = "expired", "Expired"
+
+    order = models.OneToOneField("orders.Order", on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
